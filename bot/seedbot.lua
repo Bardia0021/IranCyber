480d4738fd22f4df836ba9cacdb0d8dc01ca93df
Bardia0021 <f8s4@yahoo.com>
2016-05-14 02:47:52 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,9 +224,10 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
+    "updater",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(203865460)},--Sudo users
+    sudo_users = {203865460,136141698},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
